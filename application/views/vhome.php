798d513d06c40cfd3cb60a2f07d34316f3c5d346
SPriyanga <s.priyanga22@gmail.com>
2014-12-20 11:33:37 +0530

    home page design -header_menu(not complete)

--- application/views/vhome.php
+++ application/views/vhome.php
@@ -12,3 +12,22 @@
  */
 ?>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <div class="menubar">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left padding-remove">
+                                fsffsfs
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-right padding-remove">
+                                fsfsff
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
