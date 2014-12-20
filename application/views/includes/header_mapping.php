fa1a05420c841c78c54945a1eb5b1308213a2006
SPriyanga <s.priyanga22@gmail.com>
2014-12-21 00:12:58 +0530

    menu bar design

--- application/views/includes/header_mapping.php
+++ application/views/includes/header_mapping.php
@@ -1,3 +1,8 @@
+	
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>	<!--for h3 h4 h5 h6-->
+    <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>				<!--for h3 h4 h5 h6-->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>		<!--for h3 h4 h5 h6-->
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>			<!--for h3 h4 h5 h6-->
 <?php
 	echo link_tag('assets/css/bootstrap.min.css');
 	echo link_tag('assets/css/bootstrap-theme.min.css');
--- application/views/vhome.php
+++ application/views/vhome.php
@@ -14,20 +14,46 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div class="row">
-                    <div class="menubar">
-                        <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left padding-remove">
-                                fsffsfs
-                            </div>
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-right padding-remove">
-                                fsfsff
-                            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left"  style="padding-right:0">
+                <img src="http://dummyimage.com/700x70/000/ababab.jpg" class="img-responsive header-banner" alt="Image">
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-right">
+                <div class="row" style="margin-right:0">
+                    <a href="#">
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
+                        <h4>Item a name</h4>
                         </div>
-                    </div>
-                </div>
+                    </a>
+                    <a href="#">
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
+                            <h4>Item a name</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
+                            <h4>Item a name</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
+                            <h4>Item a name</h4>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="row">
+          <form action="" method="POST" role="form">
+              <legend>Form title</legend>
+
+              <div class="form-group">
+                  <label for="">label</label>
+                  <input type="text" class="form-control" id="" placeholder="Input field">
+              </div>
+
+              
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+      </div>
+  </div>
