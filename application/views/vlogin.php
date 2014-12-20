384677208c6f68995a81e0aa20946ee57374060a
SPriyanga <s.priyanga22@gmail.com>
2014-12-20 08:26:39 +0530

    this is a quick commit

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -13,7 +13,9 @@ Created on : Jun 21, 2014, 3:38:29 PM
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>BOI-Administrator</title>
+        <meta name="description" content="<?php echo $mDescription;?>">
+        <meta name="keywords" content="<?php echo $mKeywords;?>">
+        <title>fsf</title>
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
@@ -21,13 +23,13 @@ Created on : Jun 21, 2014, 3:38:29 PM
         <style>
             body {background:url(assets/img/login_pattern.png) repeat center 0 #fff;}
             .wrapper{background:url(assets/img/login_bgLight.png) no-repeat center 0 #222;padding:13% 0 2px; border-bottom: 6px solid rgb(163, 37, 37);}
-            .comingsoonBlk{background:rgb(182, 142, 38); padding:18px 18px 14px; max-width:444px; text-align:center; border:4px solid #fff;margin:0 auto -80px; min-height:130px; box-shadow: 0 0 188px rgb(101, 102, 101);}
+            .{background:rgb(182, 142, 38); padding:18px 18px 14px; max-width:444px; text-align:center; border:4px solid #fff;margin:0 auto -80px; min-height:130px; box-shadow: 0 0 188px rgb(101, 102, 101);}
             .clr{clear:both;}
             .comingsoonBlk button, .comingsoonBlk input{width: 100%; padding:11px 4%;}
             .comingsoonBlk h1 {line-height: 60px; margin:0}
             .copyright{padding-top:100px; color:#444; text-align:center}
-            .comingsoonBlk {background: rgb(182, 142, 38);color: #fff; border-color:rgb(46, 130, 138);}
-            .wrapper {border-bottom-color:rgb(46, 130, 138);} 
+            .comingsoonBlk {background: rgb(144, 80, 49);color: #fff; border-color:rgb(227, 141, 19);}
+            .wrapper {border-bottom-color:rgb(227, 141, 19);} 
         </style>
         <script>
             function loadmodal($modal) { // load bootstrap modals
@@ -51,7 +53,7 @@ Created on : Jun 21, 2014, 3:38:29 PM
                         <h3 class="panel-title">Sign in</h3>
                     </div>
                     <div class="panel-body">
-                        <form class="form" role="form" action="<?php echo base_url();?>admin/login_validate" method="POST">
+                        <form class="form" role="form" action="<?php echo base_url();?>home" method="POST">
                             <fieldset>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="username" name="username" type="text" required="required">
