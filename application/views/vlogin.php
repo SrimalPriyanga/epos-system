19747dcaf8d4beb6c5afeac9ab89478eef7a1e6f
SPriyanga <s.priyanga22@gmail.com>
2014-12-20 08:55:45 +0530

    login page design done

--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -23,12 +23,12 @@ Created on : Jun 21, 2014, 3:38:29 PM
         <style>
             body {background:url(assets/img/login_pattern.png) repeat center 0 #fff;}
             .wrapper{background:url(assets/img/login_bgLight.png) no-repeat center 0 #222;padding:13% 0 2px; border-bottom: 6px solid rgb(163, 37, 37);}
-            .{background:rgb(182, 142, 38); padding:18px 18px 14px; max-width:444px; text-align:center; border:4px solid #fff;margin:0 auto -80px; min-height:130px; box-shadow: 0 0 188px rgb(101, 102, 101);}
+            .loginBlk{background:rgb(182, 142, 38); padding:18px 18px 14px; max-width:444px; text-align:center; border:4px solid #fff;margin:0 auto -80px; min-height:130px; box-shadow: 0 0 188px rgb(101, 102, 101);}
             .clr{clear:both;}
-            .comingsoonBlk button, .comingsoonBlk input{width: 100%; padding:11px 4%;}
-            .comingsoonBlk h1 {line-height: 60px; margin:0}
+            .loginBlk button, .loginBlk input{width: 100%; padding:11px 4%;}
+            .loginBlk h1 {line-height: 60px; margin:0}
             .copyright{padding-top:100px; color:#444; text-align:center}
-            .comingsoonBlk {background: rgb(144, 80, 49);color: #fff; border-color:rgb(227, 141, 19);}
+            .loginBlk {background: rgb(144, 80, 49);color: #fff; border-color:rgb(227, 141, 19);}
             .wrapper {border-bottom-color:rgb(227, 141, 19);} 
         </style>
         <script>
@@ -45,7 +45,7 @@ Created on : Jun 21, 2014, 3:38:29 PM
 <?php } ?>    
         
         <div class="wrapper">
-            <div class="comingsoonBlk">
+            <div class="loginBlk">
                 <h4>Best of India<i><small style="color: #cccccc;"> / Admin</small></i></h4>
                 <h1 style="color: #333333; font-family: serif">Let's get started ...</h1>
                 <div class="panel panel-default" style="border-radius:0;">
@@ -71,8 +71,7 @@ Created on : Jun 21, 2014, 3:38:29 PM
         </div>
         <div class="copyright">	best of india &copy; 2014.</div>
         
-        
-<!-- Delete Modal Start-->
+<!-- Login Failed Modal Start-->
 <div class="modal fade" id="loginfailed" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -91,7 +90,7 @@ Created on : Jun 21, 2014, 3:38:29 PM
         </div>
     </div>
 </div> 
-<!-- Delete Modal End-->
+<!--  Login Failed Modal End-->
         
     </body>
 </html>
