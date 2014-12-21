11b43d405505f6b84fe4cd300b286387c5785beb
SPriyanga <s.priyanga22@gmail.com>
2014-12-21 11:06:54 +0530

    Admin home designs Done

--- application/views/vhome.php
+++ application/views/vhome.php
@@ -13,47 +13,82 @@
 ?>
 <body>
     <div class="container">
-        <div class="row">
+    <!-- Home Header bar start -->
+    <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-left"  style="padding-right:0">
-                <img src="http://dummyimage.com/700x70/000/ababab.jpg" class="img-responsive header-banner" alt="Image">
+                <img src="http://dummyimage.com/700x75/000/ababab.jpg" class="img-responsive header-banner" alt="Image">
             </div>
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pull-right">
                 <div class="row" style="margin-right:0">
                     <a href="#">
                         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
-                        <h4>Item a name</h4>
+                            <h4>Transactions</h4>
                         </div>
                     </a>
                     <a href="#">
                         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
-                            <h4>Item a name</h4>
+                            <h4>Receipt</h4>
                         </div>
                     </a>
                     <a href="#">
                         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
-                            <h4>Item a name</h4>
+                            <h4>Reporting</h4>
                         </div>
                     </a>
                     <a href="#">
                         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 menu-items">
-                            <h4>Item a name</h4>
+                            <h4>Log Out</h4>
                         </div>
                     </a>
                 </div>
             </div>
         </div>
+        <!-- Home Header bar End -->
+        <br>
+        <!-- Home Content start -->
         <div class="row">
-          <form action="" method="POST" role="form">
-              <legend>Form title</legend>
-
-              <div class="form-group">
-                  <label for="">label</label>
-                  <input type="text" class="form-control" id="" placeholder="Input field">
-              </div>
-
-              
-
-              <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-      </div>
-  </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div id="home-block">
+                    <div class="table-responsive">
+                        <table id="home-table" class="table table-hover">
+                            <tbody>
+                                <tr style="border-left:10px solid #FFFF66;">
+                                    <td><h1>Daily Transactions</h1>             <!-- Navigation + Block1 -->
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                    </td>
+                                    <td class="alert-warning">
+                                        <a href="#">
+                                            <span class="glyphicon glyphicon glyphicon-chevron-right btn btn-lg"></span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr style="border-left:10px solid #FFCC00;">
+                                    <td><h1>Receipt Genarater</h1>              <!-- Navigation + Block2 -->
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                    </td>
+                                    <td class="alert-warning">
+                                        <a href="#">
+                                            <span class="glyphicon glyphicon glyphicon-chevron-right btn btn-lg"></span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr style="border-left:10px solid #FF9900;">
+                                    <td><h1>Reporting</h1>                      <!-- Navigation + Block3 -->
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                    </td>
+                                    <td class="alert-warning">
+                                        <a href="#">
+                                            <span class="glyphicon glyphicon glyphicon-chevron-right btn btn-lg"></span>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>  <!-- Home-block end -->
+            </div>
+        </div>  <!-- Home Content end -->
+    </div>
