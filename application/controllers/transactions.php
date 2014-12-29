69510bc384d21ac178c4d4cb9ae69346a7f9f4c1
SPriyanga <s.priyanga22@gmail.com>
2014-12-29 18:59:42 +0530

    untracked deleted files

--- application/controllers/transactions.php
+++ application/controllers/transactions.php
@@ -20,7 +20,7 @@ class Transactions extends CI_Controller {
 		$this->load->model('m_transactions');
 		$order_details['content'] = $this->m_transactions->retrieve_order_details();
 		
-		if (null != $order_details['content']) {	// Check whether results is available or not
+		if (null != $order_details['content']) {	// Check whether orders are available or not by date
 			$order_details['num_of_orders'] = 1;
 			$temp_receipt_id = $order_details['content'][0]->receipt_id;
 			foreach ($order_details['content'] as $value) {
